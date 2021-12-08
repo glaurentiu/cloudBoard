f73268d616d8e5b4647a5c4189e9436fa6cee4e8
Alex G <g.laurentiu@outlook.com>
2021-12-08 18:59:34 +0200

    get fltered materials

--- src/app/components/add-raport/add-raport.component.ts
+++ src/app/components/add-raport/add-raport.component.ts
@@ -1,47 +1,48 @@
-import { Component, OnInit, Output, EventEmitter , Input } from '@angular/core';
-import { Raport } from '../../Raport';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { MaterialService } from 'src/app/services/material.service';
+import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
+import { Raport } from "../../Raport";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { MaterialService } from "src/app/services/material.service";
+import { Material } from "../../Material";
 
 @Component({
-  selector: 'app-add-raport',
-  templateUrl: './add-raport.component.html',
-  styleUrls: ['./add-raport.component.css'],
+  selector: "app-add-raport",
+  templateUrl: "./add-raport.component.html",
+  styleUrls: ["./add-raport.component.css"],
 })
 export class AddRaportComponent implements OnInit {
   @Input() projectId?: Number;
 
   @Output() onAddRaport: EventEmitter<Raport> = new EventEmitter();
   addRaportForm: FormGroup = new FormGroup({});
-
+  materialsParsed: Material[] = [];
   projects?: string[];
-  projectClient: string = '';
-  team = '';
+  projectClient: string = "";
+  team = "";
   materials?: string[];
-  material = '';
+  material = "";
+  materialsFiltered: Material[] = [];
 
   constructor(
     private fb: FormBuilder,
-    private materialService: MaterialService,
-   
+    private materialService: MaterialService
   ) {}
 
   ngOnInit(): void {
+    this.getMaterials();
     this.initializeForm();
     this.materialService.getMaterials().subscribe((materials) => {
       let mat = materials;
       this.materials = mat.map((el) => `${el.text}`);
     });
-
   }
 
   initializeForm(): void {
     this.addRaportForm = this.fb.group({
-      projectAndClient: '',
-      date: '',
-      team: '',
+      projectAndClient: "",
+      date: "",
+      team: "",
       materialsUsed: [],
-      description: '',
+      description: "",
     });
   }
 
@@ -52,9 +53,26 @@ export class AddRaportComponent implements OnInit {
       team: this.addRaportForm.value.team,
       materialsUsed: this.addRaportForm.value.materialsUsed,
       description: this.addRaportForm.value.description,
-      projectId: this.projectId
+      projectId: this.projectId,
     };
+  
+    this.filterMaterials();
+  
     this.onAddRaport.emit(newRaport);
     this.addRaportForm.reset();
+  }
+  getMaterials() {
+    this.materialService
+      .getMaterials()
+      .subscribe((materials) => (this.materialsParsed = materials));
+  }
+  filterMaterials() {
+    
+    this.materialsParsed.forEach((material) => {
+      if (this.addRaportForm.value.materialsUsed.includes(material.text)) {
+        this.materialsFiltered.push(material);
+      }
+    });
+    
   }
 }
