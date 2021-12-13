181b12d2a6e9a7866e5451ae2543be4302d46dd1
Alex G <g.laurentiu@outlook.com>
2021-12-13 21:13:34 +0200

    modified add-request-raport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
@@ -98,7 +100,8 @@ const appRoutes: Routes = [
     MatInputModule,
     NgbModule,
     MatExpansionModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
